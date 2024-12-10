a08837549d3d34d14eea6fc69ff2ba00edba620b
aliahesham <aliahesham2003@gmail.com>
2024-12-10 04:30:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const panals = document.querySelectorAll(".panal");
+
+panals.forEach((panal) => {
+  panal.addEventListener("click", () => {
+    rmoveActive();
+    panal.classList.add("active");   
+
+
+  });
+}); 
+  
+    function rmoveActive() {
+  panals.forEach((panal) => {
+    panal.classList.remove("active");
+  });
+}
